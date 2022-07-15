a95dba1662dc4d499ffb810638f770639dff0a9e
mdevansh <mdevansh28@gmail.com>
2022-07-15 23:59:56 +0530

    max_consecutive_ones

---
+++ wix_engineering_interview/unique_email_addresses.java
@@ -0,0 +1,23 @@
+package wix_engineering_interview;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Question Link : https://leetcode.com/explore/interview/card/wix-engineering/651/arrays-and-strings/4154/
+ */
+
+public class unique_email_addresses {
+    class Solution {
+        public int numUniqueEmails(String[] emails) {
+            List uniqueEmails = new ArrayList<String>();
+            for(int i=0; i< emails.length;++i){
+                String[] parts = emails[i].split("@", 2);
+                String[] parts2 = parts[0].split("\\+", 2);
+                String s1 = parts2[0].replace(".", "");
+                if(!uniqueEmails.contains(s1))  uniqueEmails.add(s1);
+            }
+            return uniqueEmails.size();
+        }
+    }
+}
